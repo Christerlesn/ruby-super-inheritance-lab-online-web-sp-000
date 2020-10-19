3da5b4e1603b13319b6241aa7905d79e31b0679a
Christerle Nounoute <christerlesn@gmail.com>
2020-10-19 05:53:44 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,9 +2,9 @@ require_relative "./student.rb"
 class ChattyStudent < Student
   def hello
     super
-    $stdout.print "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Wal
-king Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoile
-rs? Okay well let me just tell you who died...\n"
+    $stdout.print "Hey there! I'm so excited to learn stuff.\nHow are you doi
+ng today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any s
+poilers? Okay well let me just tell you who died...\n"
   end
 
   def raise_hand
